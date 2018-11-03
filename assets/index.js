9ff2821a2e71c209b11f8fa3af919fd20e4b744d
akihira207 <tsunose.developer@gmail.com>
2018-11-03 15:26:27 +0900

    delete unused variable

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@ $(function(){
     /**
      * 日報の生成ロジック
      */
-    $('.generate').on('click', function(e){
+    $('.generate').on('click', function(){
         var kousu    = $('.kousu01').val(),
             classify = $('.classify01').val(),
             detail   = $('.detail01').val(),
